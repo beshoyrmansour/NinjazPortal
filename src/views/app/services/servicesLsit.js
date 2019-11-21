c4f6b2cec2d9a6a977c8eb5a5b9f47ad13e60cf8
Beshoy Mansour <beshoy.r.mansour@gmail.com>
2019-11-21 11:18:18 +0200

    hasAdvancedOption:false

--- src/views/app/services/servicesLsit.js
+++ src/views/app/services/servicesLsit.js
@@ -15,53 +15,57 @@ const ServicesLsit = props => {
     {
       id: "c1",
       title: "Translation Services",
+      hasAdvancedOption:true,
       services: [
-        { title: "Legal", id: "c1s1" },
-        { title: "Technical", id: "c1s2" },
-        { title: "Website localization", id: "c1s3" },
-        { title: "Marketing", id: "c1s4" },
-        { title: "Medical", id: "c1s5" },
-        { title: "Literature", id: "c1s6" },
-        { title: "Other", id: "c2s7" }
+        { title: "Legal", id: "c1s1" ,hasAdvancedOption:true,},
+        { title: "Technical", id: "c1s2" ,hasAdvancedOption:true,},
+        { title: "Website localization", id: "c1s3" ,hasAdvancedOption:true,},
+        { title: "Marketing", id: "c1s4" ,hasAdvancedOption:true,},
+        { title: "Medical", id: "c1s5" ,hasAdvancedOption:true,},
+        { title: "Literature", id: "c1s6" ,hasAdvancedOption:true,},
+        { title: "Other", id: "c2s7" ,hasAdvancedOption:true,},
       ]
     },
     {
       id: "c2",
       title: "Document Clearance",
+      hasAdvancedOption:false,
       services: [
-        { title: "PRO", id: "c2s1" },
-        { title: "Company setup and formation", id: "c2s2" },
-        { title: "Attestation and certification", id: "c2s3" },
-        { title: "Registration for Patent, copyright and trademark", id: "c2s4" },
-        { title: "Product Registration", id: "c2s5" },
-        { title: "Other", id: "c2s6" }
+        { title: "PRO", id: "c2s1" ,hasAdvancedOption:false},
+        { title: "Company setup and formation", id: "c2s2" ,hasAdvancedOption:false},
+        { title: "Attestation and certification", id: "c2s3" ,hasAdvancedOption:false},
+        { title: "Registration for Patent, copyright and trademark", id: "c2s4" ,hasAdvancedOption:false},
+        { title: "Product Registration", id: "c2s5" ,hasAdvancedOption:false},
+        { title: "Other", id: "c2s6" ,hasAdvancedOption:false},
       ]
     },
     {
       id: "c3",
       title: "Legal Advice",
+      hasAdvancedOption:false,
       services: [
-        { title: "Commercial and Corporate low", id: "c3s1" },
-        { title: "Company incorporation", id: "c3s2" },
-        { title: "Dispute resolution", id: "c3s3" },
-        { title: "Immigration and citizenship", id: "c3s4" },
-        { title: "Intellectual property low", id: "c3s5" },
-        { title: "Real estate and property low", id: "c3s6" },
-        { title: "Arbitration low", id: "c3s7" },
-        { title: "Other", id: "c3s8" }
+        { title: "Commercial and Corporate low", id: "c3s1" ,hasAdvancedOption:false},
+        { title: "Company incorporation", id: "c3s2" ,hasAdvancedOption:false},
+        { title: "Dispute resolution", id: "c3s3" ,hasAdvancedOption:false},
+        { title: "Immigration and citizenship", id: "c3s4" ,hasAdvancedOption:false},
+        { title: "Intellectual property low", id: "c3s5" ,hasAdvancedOption:false},
+        { title: "Real estate and property low", id: "c3s6" ,hasAdvancedOption:false},
+        { title: "Arbitration low", id: "c3s7" ,hasAdvancedOption:false},
+        { title: "Other", id: "c3s8" ,hasAdvancedOption:false},
       ]
     },
     {
       id: "c4",
       title: "SME Services",
+      hasAdvancedOption:false,
       services: [
-        { title: "Outsourcing", id: "c4s1" },
-        { title: "VAT registration", id: "c4s2" },
-        { title: "SME Setup", id: "c4s3" },
-        { title: "Business accounting", id: "c4s4" },
-        { title: "Website/app creation", id: "c4s5" },
-        { title: "PRO", id: "c4s6" },
-        { title: "Other", id: "c4s7" }
+        { title: "Outsourcing", id: "c4s1" , hasAdvancedOption:false},
+        { title: "VAT registration", id: "c4s2" , hasAdvancedOption:false},
+        { title: "SME Setup", id: "c4s3" , hasAdvancedOption:false},
+        { title: "Business accounting", id: "c4s4" , hasAdvancedOption:false},
+        { title: "Website/app creation", id: "c4s5" , hasAdvancedOption:false},
+        { title: "PRO", id: "c4s6" , hasAdvancedOption:false},
+        { title: "Other", id: "c4s7" , hasAdvancedOption:false},
       ]
     }
   ];
@@ -105,9 +109,10 @@ const ServicesLsit = props => {
                               <Button color="primary" className=" " onClick={() => RedirectToServvices(service, "easy")}>
                                 Easy Mode
                               </Button>
+                              {service.hasAdvancedOption && 
                               <Button color="outline-dark" className=" ml-2" onClick={() => RedirectToServvices(service, "advanced")}>
                                 advanced Mode
-                              </Button>
+                              </Button>}
                             </div>
                           </CardBody>
                         </Card>
